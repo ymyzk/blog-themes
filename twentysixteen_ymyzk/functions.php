01f9bafc3e99000f3266bf33eccd363668bea929
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-08-18 01:55:12 +0900

    Fix coding style

--- twentysixteen_ymyzk/functions.php
+++ twentysixteen_ymyzk/functions.php
@@ -1,15 +1,15 @@
 <?php
 
 /* Inherit a parent theme stylesheet */
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 /* Remove Jetpack Share */
 function jptweak_remove_share() {
-    remove_filter( 'the_content', 'sharing_display',19 );
-    remove_filter( 'the_excerpt', 'sharing_display',19 );
+    remove_filter( 'the_content', 'sharing_display', 19 );
+    remove_filter( 'the_excerpt', 'sharing_display', 19 );
     if ( class_exists( 'Jetpack_Likes' ) ) {
         remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
     }
