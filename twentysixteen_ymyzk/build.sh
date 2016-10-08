d61fa7ca3afe3aa1ce40f0745f8683806f7d3ba2
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-10-08 18:20:46 +0900

    Use custom build theme of highlight.js

---
+++ twentysixteen_ymyzk/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+languages=(
+  # Common
+  'bash'
+  'cs' # C#
+  'css'
+  'diff'
+  'http'
+  'ini'
+  'json'
+  'java'
+  'javascript'
+  'makefile'
+  'markdown'
+  'nginx'
+  'objective-c'
+  'php'
+  'perl'
+  'python'
+  'ruby'
+  'sql'
+  'xml' # including HTML
+  # Others
+  'django'
+  'dockerfile'
+  'ocaml'
+  'swift'
+  'tex'
+  'yaml'
+)
+
+cd $(dirname $0)
+
+cd highlight.js/
+npm install
+
+node ./tools/build.js -n ${languages[*]}
+cp ./build/highlight.pack.js ../js/highlight.js
+
+node ./tools/build.js ${languages[*]}
+cp ./build/highlight.pack.js ../js/highlight.min.js
+
+node ./tools/build.js -t cdn
+cp ./build/styles/monokai.min.css ../css/
+
+cd ..
