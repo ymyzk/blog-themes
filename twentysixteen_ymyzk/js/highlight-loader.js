8059ab3e3fccccd2ca147812f565239e22d739e7
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-10-08 17:36:23 +0900

    Add a custom highlight.js language for plain text

--- twentysixteen_ymyzk/js/highlight-loader.js
+++ twentysixteen_ymyzk/js/highlight-loader.js
@@ -1,6 +1,11 @@
 var $ = jQuery;
 
 $(document).ready(function() {
+  // Add a custom language for plain text
+  hljs.registerLanguage("custom-plain", function(hljs) {
+    return {};
+  });
+
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
