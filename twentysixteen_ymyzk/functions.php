b6291836245e63860fbb59bbca10166fa967ac6a
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-10-08 18:02:28 +0900

    Add custom build highlight.js

--- twentysixteen_ymyzk/functions.php
+++ twentysixteen_ymyzk/functions.php
@@ -28,6 +28,6 @@ add_action( 'loop_start', 'jetpackme_remove_rp' );
 add_action( 'wp_enqueue_scripts', 'append_highlight_js' );
 function append_highlight_js() {
     wp_enqueue_style( 'highlight.js', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/styles/monokai.min.css' );
-    wp_enqueue_script( 'highlight.js', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js', array(), false, true );
+    wp_enqueue_script( 'highlight.js', get_stylesheet_directory_uri() . '/js/highlight.pack.js', array(), false, true );
     wp_enqueue_script( 'highlight-loader.js', get_stylesheet_directory_uri() . '/js/highlight-loader.js', array('jquery', 'highlight.js'), false, true );
 }
