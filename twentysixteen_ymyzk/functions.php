6ee1eab256d5eef594f960e5ad978fd5026fd001
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-12-08 01:26:24 +0900

    Merge parent style file on build

--- twentysixteen_ymyzk/functions.php
+++ twentysixteen_ymyzk/functions.php
@@ -1,11 +1,5 @@
 <?php
 
-/* Inherit a parent theme stylesheet */
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-
 /* Remove Jetpack Share */
 function jptweak_remove_share() {
     remove_filter( 'the_content', 'sharing_display', 19 );
