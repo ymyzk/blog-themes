26950a032a1275fe7083146d42f9601cb806f33e
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-12-12 02:57:40 +0900

    Add PV

--- twentysixteen_ymyzk/js/main.js
+++ twentysixteen_ymyzk/js/main.js
@@ -21,7 +21,8 @@ literal:"true false null",built_in:"array bigint binary bit blob boolean char ch
 }, false);
 if (typeof WPCOM_sharing_counts !== "undefined") {
   for (var url in WPCOM_sharing_counts) {
-    var $shareTwitter = jQuery('a[data-shared=sharing-twitter-' + WPCOM_sharing_counts[url] + ']');
+    var postId = WPCOM_sharing_counts[url];
+    var $shareTwitter = jQuery('a[data-shared=sharing-twitter-' + postId + ']');
 
     jQuery.ajax({
       url     : "https://b.hatena.ne.jp/entry.count",
@@ -32,6 +33,20 @@ if (typeof WPCOM_sharing_counts !== "undefined") {
         var $shareHatena = $shareTwitter.parent().nextAll(".share-custom-hatena").children("a");
         $shareHatena.find('span:has(.share-count)').remove();
         $shareHatena.append('<span style="padding: 0 !important"><span class="share-count" style="padding: 1px 3px !important; line-height: 9px; width: auto; height: auto;">' + WPCOMSharing.format_count(count) + '</span></span>');
+      }
+    });
+
+    jQuery.ajax({
+      url     : "/wpcom-stats-proxy/stats/post/" + postId,
+      success : function(data) {
+        if (typeof data === "undefined") return;
+        var count = data.views;
+        if (typeof count === "undefined" || count <= 0) return;
+        var $sharePV = $shareTwitter.parent().nextAll(".share-custom-pv").children("a");
+        $sharePV.attr("href", "#");
+        $sharePV.removeAttr("target");
+        $sharePV.find('span:has(.share-count)').remove();
+        $sharePV.append('<span style="padding: 0 !important"><span class="share-count" style="padding: 1px 3px !important; line-height: 9px; width: auto; height: auto;">' + WPCOMSharing.format_count(count) + '</span></span>');
       }
     });
   }
