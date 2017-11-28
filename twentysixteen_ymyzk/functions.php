f0d818ee4a849994d4d2aaff8027ff582aa9f689
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-11-28 17:13:07 +0900

    Bump version number

--- twentysixteen_ymyzk/functions.php
+++ twentysixteen_ymyzk/functions.php
@@ -2,7 +2,7 @@
 
 /* Specify version number */
 function specify_custom_version( $styles ) {
-    $styles->default_version = 20171117;
+    $styles->default_version = 20171128;
 }
 add_action( 'wp_default_styles', 'specify_custom_version' );
 
